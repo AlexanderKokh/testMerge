a14f9dca9d27c849218e78e578524a1f8d950732
Александр Кох <AKokh@inno.tech>
2024-02-13 17:45:16 +0300

    example

---
+++ testMergeexample/testMergeexample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testMergeexample
+//
+//  Created by Кох Александр Станиславович on 13.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
