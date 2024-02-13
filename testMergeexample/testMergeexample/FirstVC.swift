27c469c011a7080e19cab089e3e3b5bf6511c903
Александр Кох <AKokh@inno.tech>
2024-02-13 18:11:56 +0300

    refactoring

--- testMergeexample/testMergeexample/FirstVC.swift
+++ testMergeexample/testMergeexample/FirstVC.swift
@@ -7,19 +7,23 @@
 
 import UIKit
 
-class FirstVC: UIViewController {
+final class FirstVC: UIViewController {
     
-    let label = UILabel()
+    private let label = UILabel()
       
       override func viewDidLoad() {
           super.viewDidLoad()
-          view.backgroundColor = .white
-          title = "Первый экран"
-          
-          view.addSubview(label)
-          label.frame = .init(x: view.frame.midX - 100, y: view.frame.midY - 50, width: 200, height: 100)
-          label.text = "Вы на первом экране"
-          
-          printTitle()
+          configure()
       }
+    
+    private func configure() {
+        view.backgroundColor = .white
+        title = "Первый экран"
+        
+        view.addSubview(label)
+        label.frame = .init(x: view.frame.midX - 100, y: view.frame.midY - 50, width: 200, height: 100)
+        label.text = "Вы на первом экране"
+        
+        printTitle()
+    }
 }
