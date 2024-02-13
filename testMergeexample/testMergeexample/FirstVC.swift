e4f7a3ec945911880c3b77eb07abf66b14e15e3c
Александр Кох <AKokh@inno.tech>
2024-02-13 18:24:57 +0300

    add new field at firstVC

--- testMergeexample/testMergeexample/FirstVC.swift
+++ testMergeexample/testMergeexample/FirstVC.swift
@@ -9,17 +9,29 @@ import UIKit
 
 class FirstVC: UIViewController {
     
-    let label = UILabel()
-      
-      override func viewDidLoad() {
-          super.viewDidLoad()
-          view.backgroundColor = .white
-          title = "Первый экран"
-          
-          view.addSubview(label)
-          label.frame = .init(x: view.frame.midX - 100, y: view.frame.midY - 50, width: 200, height: 100)
-          label.text = "Вы на первом экране"
-          
-          printTitle()
-      }
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Вы на первом экране"
+        label.frame = .init(x: view.frame.midX - 100, y: view.frame.midY - 50, width: 200, height: 100)
+        return label
+    }()
+    
+    lazy var greetingLabel: UILabel = {
+        let label = UILabel()
+        label.frame = .init(x: view.frame.midX - 100, y: view.frame.midY - 150, width: 300, height: 100)
+        label.font = .boldSystemFont(ofSize: 30)
+        label.textAlignment = .center
+        label.textColor = .red
+        label.text = getMainTitle()
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Первый экран"
+        
+        view.addSubview(label)
+        view.addSubview(greetingLabel)
+    }
 }
