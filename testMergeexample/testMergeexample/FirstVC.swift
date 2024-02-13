bf2d4b219ab1213d8482cfe8f2407b674dc5a6b8
Александр Кох <AKokh@inno.tech>
2024-02-13 17:53:01 +0300

    configureFirstVC

--- testMergeexample/testMergeexample/FirstVC.swift
+++ testMergeexample/testMergeexample/FirstVC.swift
@@ -9,7 +9,17 @@ import UIKit
 
 class FirstVC: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    let label = UILabel()
+      
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          view.backgroundColor = .white
+          title = "Первый экран"
+          
+          view.addSubview(label)
+          label.frame = .init(x: view.frame.midX - 100, y: view.frame.midY - 50, width: 200, height: 100)
+          label.text = "Вы на первом экране"
+          
+          printTitle()
+      }
 }
